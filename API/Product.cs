871499f877fbdf0133125cf7d38292f8141bcec7
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-06-26 17:59:55 +0200

    Product model add

---
+++ API/Product.cs
@@ -0,0 +1,10 @@
+namespace API.Models
+{
+    public class Product
+    {
+        public double Kcal {get;set;}
+        public double Protein {get;set;}
+        public double Carbohydrates {get;set;}
+        public double Fat {get;set;}
+    }
+}
