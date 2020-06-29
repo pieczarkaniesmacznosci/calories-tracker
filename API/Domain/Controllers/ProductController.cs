4d89caf8aa51f473ec0cb2605469db77404672aa
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-06-29 11:30:41 +0200

    Solution modify, build correct

---
+++ API/Domain/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>(){
+                new Product{
+                    Kcal = 101d,
+                    Protein = 101d,
+                    Fat = 3d,
+                    Carbohydrates = 2d
+                }
+            };
+        }
+    }
+}
