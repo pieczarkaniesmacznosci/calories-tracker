ca40eb04051fcc2b5809958a6cc2aec445c7162e
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-09-23 21:10:12 +0200

    Mapping profile change

--- API/Web/MappingProfile.cs
+++ API/Web/MappingProfile.cs
@@ -7,18 +7,21 @@ namespace API.Web
     public class MappingProfile : Profile 
     {
         public MappingProfile() {
-            CreateMap<Product, ProductDto>();
-            CreateMap<ProductDto, Product>();
+            CreateMap<Product, ProductDto>()
+            .ForMember(x=>x.Id,opt =>opt.Ignore());
+            CreateMap<ProductDto, Product>()
+            .ForMember(x=>x.Id,opt =>opt.Ignore());
             CreateMap<Meal, MealDto>()
                 .ForMember(
                     dest=>dest.MealProducts, 
-                    opt=>opt.MapFrom(src=>src.MealProducts));
+                    opt=>opt.MapFrom(src=>src.MealProducts))
+            .ForMember(x=>x.Id,opt =>opt.Ignore());
             CreateMap<MealDto, Meal>()
                 .ForMember(
                     dest=>dest.MealProducts, 
                     opt=>opt.MapFrom(src=>src.MealProducts));
-            CreateMap<MealDto, Meal>();
-            CreateMap<MealProduct, MealProductDto>();
+            CreateMap<MealProduct, MealProductDto>()
+            .ForMember(x=>x.Id,opt =>opt.Ignore());
             CreateMap<MealProductDto, MealProduct>();
         }
     }
