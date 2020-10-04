620583fd2565e35e104ae0949eec89fb7ebbc151
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-10-04 13:12:40 +0200

    Basic View with Vm create, debug config create

---
+++ App/Tracly/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Tracly.Models;
+using Tracly.ViewModels;
+
+namespace Tracly.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductRepository _productrepository;
+
+        public ProductController(ILogger<ProductController> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            _productrepository = productRepository;
+        }
+        public IActionResult List()
+        {
+            var productListVM = new ProductListViewModel();
+
+            productListVM.Title = "Products";
+            productListVM.Products = _productrepository.AllProducts;
+
+            return View(productListVM);
+        }
+    }
+}
